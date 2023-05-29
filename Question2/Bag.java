822ed9d862ac93389d531a42144da69aa36e3598
Hussein Abdikarim Hussein <131241911+Husseinabdikarim@users.noreply.github.com>
2023-05-29 13:00:42 +0300

    Update Bag.java

--- Question2/Bag.java
+++ Question2/Bag.java
@@ -7,7 +7,6 @@ public class Bag<Item> implements Iterable<Integer> {
 //-----------------------------------------------------
 // Title: Question 2
 // Author: Hussein Abdikarim Hussein
-// ID: 99230997266
 // Section: 1
 // Assignment: 1
 // Description: This is a generic Bag class that holds
